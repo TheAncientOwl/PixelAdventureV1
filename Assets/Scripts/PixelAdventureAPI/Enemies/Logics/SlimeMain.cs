1ba5bb36235e5bfbbbeb2200eac6df9137e5888f
TheAncientOwl <dele.alex.o.o@gmail.com>
2020-07-28 00:28:10 +0300

    Added Slime

---
+++ Assets/Scripts/PixelAdventureAPI/Enemies/Logics/SlimeMain.cs
@@ -0,0 +1,34 @@
+﻿using PixelAdventureAPI.PlayerLogics;
+using PixelAdventureAPI.Enemies.API;
+using UnityEngine;
+
+namespace PixelAdventureAPI.Enemies.Logics
+{
+    /// <summary>
+    /// Manages SimpleCollision
+    /// </summary>
+    public class SlimeMain : MonoBehaviour
+    {
+        [Header("On Death")]
+        [SerializeField] private float m_PlayerJumpVelocity = 9f;
+        [SerializeField] private int m_Points = 5;
+
+        public void Start()
+        {
+            SimpleCollision collision = GetComponent<SimpleCollision>();
+            collision.AddOnDeath(OnDeathEvent);
+        }
+
+        private void OnDeathEvent()
+        {
+            PlayerScore.GetInstance().Value += m_Points;
+            m_Points = 0;
+            GetComponent<Animator>().SetTrigger("hit");
+            GetComponent<BasicMovement>().StopAllCoroutines();
+            Rigidbody2D playerRigidbody2d = PlayerMovement.GetInstance().GetRigidbody2D();
+            playerRigidbody2d.velocity = new Vector2(playerRigidbody2d.velocity.x, m_PlayerJumpVelocity);
+            Destroy(this.gameObject, 0.49f);
+        }
+
+    }
+}
