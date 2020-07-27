c5878cce5756d6e22e39f0e40e90e679fa00dcd1
TheAncientOwl <dele.alex.o.o@gmail.com>
2020-07-28 00:05:43 +0300

    Added TargetX

---
+++ Assets/Scripts/PixelAdventureAPI/Others/TargetX.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PixelAdventureAPI.Others
+{
+    public struct TargetX
+    {
+        public float left, right;
+        public TargetX(float left, float right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+    };
+}
+
